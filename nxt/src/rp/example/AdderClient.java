a8f31913a94285bd7e44561378ea529d441af9be
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2015-11-23 22:30:04 +0000

    Working multi-nxt example

---
+++ nxt/src/rp/example/AdderClient.java
@@ -0,0 +1,43 @@
+package rp.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.nxt.Button;
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
+
+public class AdderClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Waiting for Bluetooth connection...");
+		BTConnection connection = Bluetooth.waitForConnection();
+		System.out.println("OK!");
+
+		DataInputStream inputStream = connection.openDataInputStream();
+		DataOutputStream outputStream = connection.openDataOutputStream();
+
+		boolean run = true;
+		while (run) {
+
+			try {
+				int input = inputStream.readInt();
+				if (input != 0) {
+					outputStream.writeInt(input + 1);
+					outputStream.flush();
+				} else {
+					run = false;
+				}
+			} catch (IOException e) {
+				System.out.println("Exception: " + e.getClass());
+				run = false;
+			}
+		}
+	}
+
+}
